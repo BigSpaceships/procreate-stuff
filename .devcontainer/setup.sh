8ccfddd11057a5fd57665e5ccded8180c63ae771
BigSpaceships <bigspaceships56@gmail.com>
2024-06-02 16:48:20 -0400

    CHORE: added devcontainer config

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,9 @@
+## update and install some things we should probably have
+apt-get update
+apt-get install -y \
+    pkg-config
+
+## Install rustup and common components
+curl https://sh.rustup.rs -sSf | sh -s -- -y 
+
+source "$HOME/.cargo/env"
